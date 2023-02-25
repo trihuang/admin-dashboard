72d97222343f7c347f46a3634a4e73af05751d13
trihuang <124452877+trihuang@users.noreply.github.com>
2023-02-26 01:36:47 +0800

    Add script to make icons change color when clicked

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const notificationIcon = document.querySelector('.icons-container span');
+const cardIcons = document.querySelectorAll('.icons .material-icons-outlined');
 
 notificationIcon.addEventListener('click', changeColor);
+cardIcons.forEach((cardIcon) => {
+    cardIcon.addEventListener('click', changeColor);
+});
 
 function changeColor(e) {
     let color = e.target.style.color;
